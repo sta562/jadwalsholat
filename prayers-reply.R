586d2c51ace05c1d357011d65dfeb9c3f6e3d768
Alfa Nugraha <alfa.nugraha4@gmail.com>
2022-03-31 10:41:11 +0100

    Checking if mention exists

--- prayers-reply.R
+++ prayers-reply.R
@@ -5,22 +5,6 @@ library(rtweet)
 message("Set timezone")
 Sys.setenv(TZ = "Asia/Bangkok")
 
-message("Connect to ElephantSQL database server")
-con <- dbConnect(
-  dbDriver("PostgreSQL"),
-  dbname = Sys.getenv("ELEPHANT_SQL_DBNAME"),
-  host = Sys.getenv("ELEPHANT_SQL_HOST"),
-  port = 5432,
-  user = Sys.getenv("ELEPHANT_SQL_USER"),
-  password = Sys.getenv("ELEPHANT_SQL_PASSWORD")
-)
-
-message("Retrieve the information from table `public`.`prayer`")
-prayer <- dbReadTable(con, "prayer")
-
-today <- format(Sys.time(), "%d-%m-%Y")
-tweetprayer <- prayer[which(today==prayer$date),]
-
 token <- create_token(
   app = "Sholat-Bot",
   consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
@@ -30,24 +14,47 @@ token <- create_token(
 )
 
 mention_list <- get_mentions("SholatBot")
-mention_who <- mention_list$status_id
-mention_info <- lookup_statuses(mention_who)
 
-## not finished yet
-status_details <- paste0("@", mention_info$screen_name, "\n",
-  "🕌 Waktu sholat untuk ", tweetprayer$city, " dan sekitarnya\n",
-  "Imsak", " ", tweetprayer['imsak'], "\n",
-  "Subuh", " ", tweetprayer['fajr'], "\n",
-  "Dhuhr", " ", tweetprayer['dhuhr'], "\n",
-  "Asr", " ", tweetprayer['asr'], "\n",
-  "Maghrib", " ", tweetprayer['maghrib'], "\n",
-  "Isha", " ", tweetprayer['isha'], "\n",
-  "\n")
+## reply mention features
+if(nrow(mention_list) > 0){
+  mention_who <- mention_list$status_id
+  mention_info <- lookup_statuses(mention_who)
 
-message("Post the status to twitter")
-## Post the image to Twitter
-post_tweet(
-  status = status_details,
-  token = token
-)
+  message("Connect to ElephantSQL database server")
+  con <- dbConnect(
+    dbDriver("PostgreSQL"),
+    dbname = Sys.getenv("ELEPHANT_SQL_DBNAME"),
+    host = Sys.getenv("ELEPHANT_SQL_HOST"),
+    port = 5432,
+    user = Sys.getenv("ELEPHANT_SQL_USER"),
+    password = Sys.getenv("ELEPHANT_SQL_PASSWORD")
+  )
+  
+  message("Retrieve the information from table `public`.`prayer`")
+  prayer <- dbReadTable(con, "prayer")
+  
+  today <- format(Sys.time(), "%d-%m-%Y")
+  tweetprayer <- prayer[which(today==prayer$date),]
+  
+  status_details <- paste0("@", mention_info$screen_name, " ",
+    "🕌 Waktu sholat untuk ", tweetprayer$city, " dan sekitarnya\n",
+    "Imsak", " ", tweetprayer['imsak'], "\n",
+    "Subuh", " ", tweetprayer['fajr'], "\n",
+    "Dhuhr", " ", tweetprayer['dhuhr'], "\n",
+    "Asr", " ", tweetprayer['asr'], "\n",
+    "Maghrib", " ", tweetprayer['maghrib'], "\n",
+    "Isha", " ", tweetprayer['isha'], "\n",
+    "\n")
+
+  message("Post the status to twitter")
+  ## Post the image to Twitter
+  post_tweet(
+    status = status_details,
+    token = token
+  )
+  
+  message("Disconnect the database")
+  on.exit(dbDisconnect(con))
+  
+}
 
